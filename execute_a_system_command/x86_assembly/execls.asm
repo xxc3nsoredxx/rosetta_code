db7f5abf94ef3bd9178196a72c5b2b69d6247b1f
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2020-03-14 04:24:50 -0500

    Executes a System Command
    x86 Assembly, execve('/bin/ls', ['/bin/ls'], NULL)

--- execute_a_system_command/x86_assembly/execls.asm
+++ execute_a_system_command/x86_assembly/execls.asm
@@ -1,1 +1,20 @@
-; Executes 'ls'
+; Executes '/bin/ls'
+; Build with:
+;   nasm -felf32 execls.asm
+;   ld -m elf_i386 execls.o -o execls
+
+global  _start
+section .text
+
+_start:
+    push ebp
+    mov ebp, esp
+
+    mov eax, 0x0B       ; sys_execve(char *str, char **args, char **envp)
+    mov ebx, .path      ; pathname
+    push DWORD .path    ; arguments [pathname]
+    lea ecx, [ebp - 4]
+    xor edx, edx        ; environment variables []
+    int 0x80            ; syscall
+.path:
+    db  '/bin/ls', 0x00, 0x00
